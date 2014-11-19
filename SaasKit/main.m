77d617e12b3d81483da3754e5fc35dd7f141e696
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2014-11-19 10:46:47 +0100

    git init + .gitignore

---
+++ SaasKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SaasKit
+//
+//  Created by Patryk Kaczmarek on 19.11.2014.
+//  Copyright (c) 2014 Netguru.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
