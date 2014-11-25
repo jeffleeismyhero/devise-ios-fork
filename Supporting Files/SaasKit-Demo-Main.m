cb72b140e97857c58b2e4cab01072489fff78832
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2014-11-25 23:13:45 +0100

    Restore the demo application

--- Supporting Files/SaasKit-Demo-Main.m
+++ Supporting Files/SaasKit-Demo-Main.m
@@ -5,10 +5,10 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "SSKAppDelegate.h"
+#import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSKAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
