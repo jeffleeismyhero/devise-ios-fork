c462e6a158433135191b84a8dde29f6f55524f4e
Wojceich Trzasko <wojciech.trzasko@gmail.com>
2014-12-19 11:25:52 +0100

    Localization for client side validation.

--- Devise Demo/DVSDemoUserDataSource.m
+++ Devise Demo/DVSDemoUserDataSource.m
@@ -38,22 +38,22 @@ static NSString * const DVSUserEmailParameter = @"email";
 
 - (NSArray *)additionalValidationRulesForLogin {
     return @[[self defaultDemoValidatorForEmail],
-             DVSValidate(DVSUserPasswordParameter).localizedPropertyName(@"Password")
-             .minLength(5).tooShort(@"is wrong.")];
+             DVSValidate(DVSUserPasswordParameter).localizedPropertyName(NSLocalizedString(@"Password", nil))
+             .minLength(5).tooShort(NSLocalizedString(@"is wrong.", nil))];
 }
 
 - (NSArray *)additionalValidationRulesForRegistration {
     return @[[self defaultDemoValidatorForEmail],
              [self defaultDemoValidatorForPassword],
-             DVSValidate(DVSDemoUserUsernameParameter).localizedPropertyName(@"Username")
+             DVSValidate(DVSDemoUserUsernameParameter).localizedPropertyName(NSLocalizedString(@"Username", nil))
                                                       .required()
-                                                      .minLength(5).tooShort(@"should have at least 5 characters."),
-             DVSValidate(DVSDemoUserFirstNameParameter).localizedPropertyName(@"First name")
+                                                      .minLength(5).tooShort(NSLocalizedString(@"should have at least 5 characters.", nil)),
+             DVSValidate(DVSDemoUserFirstNameParameter).localizedPropertyName(NSLocalizedString(@"First name", nil))
                                                        .required()
-                                                       .minLength(1).tooShort(@"can't be empty."),
-             DVSValidate(DVSDemoUserLastNameParameter).localizedPropertyName(@"Last name")
+                                                       .minLength(1).tooShort(NSLocalizedString(@"can't be empty.", nil)),
+             DVSValidate(DVSDemoUserLastNameParameter).localizedPropertyName(NSLocalizedString(@"Last name", nil))
                                                       .required()
-                                                      .minLength(1).tooShort(@"can't be empty.")
+                                                      .minLength(1).tooShort(NSLocalizedString(@"can't be empty.", nil))
              ];
 }
 
@@ -62,18 +62,18 @@ static NSString * const DVSUserEmailParameter = @"email";
 }
 
 - (NSArray *)additionalValidationRulesForPasswordChange {
-    return @[DVSValidate(DVSUserPasswordParameter).localizedPropertyName(@"New password").minLength(5).tooShort(@"should have at least 5 characters.")];
+    return @[DVSValidate(DVSUserPasswordParameter).localizedPropertyName(NSLocalizedString(@"New password", nil)).minLength(5).tooShort(NSLocalizedString(@"should have at least 5 characters.", nil))];
 }
 
 #pragma mark - Default demo validators
 
 - (DVSPropertyValidator *)defaultDemoValidatorForEmail {
-    return DVSValidate(DVSUserEmailParameter).localizedPropertyName(@"E-mail");
+    return DVSValidate(DVSUserEmailParameter).localizedPropertyName(NSLocalizedString(@"E-mail", nil));
 }
 
 - (DVSPropertyValidator *)defaultDemoValidatorForPassword {
-    return DVSValidate(DVSUserPasswordParameter).localizedPropertyName(@"Password")
-                                                .minLength(5).tooShort(@"should have at least 5 characters.");
+    return DVSValidate(DVSUserPasswordParameter).localizedPropertyName(NSLocalizedString(@"Password", nil))
+                                                .minLength(5).tooShort(NSLocalizedString(@"should have at least 5 characters.", nil));
 }
 
 @end
