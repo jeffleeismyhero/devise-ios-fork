85472106107129374ac86fd09ba5fee59cf71e4f
Wojceich Trzasko <wojciech.trzasko@gmail.com>
2014-12-19 13:40:18 +0100

    Success message for password reminder.

--- Devise Demo/DVSPasswordReminderViewController.m
+++ Devise Demo/DVSPasswordReminderViewController.m
@@ -12,7 +12,7 @@
 #import "UIAlertView+DeviseDemo.h"
 #import "DVSDemoUser.h"
 
-@interface DVSPasswordReminderViewController ()
+@interface DVSPasswordReminderViewController () <UIAlertViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
 
@@ -20,18 +20,37 @@
 
 @implementation DVSPasswordReminderViewController
 
+#pragma mark - UIControl events
+
 - (IBAction)sendButtonTouched:(UIBarButtonItem *)sender {
     DVSDemoUser *user = [DVSDemoUser new];
     
     user.email = self.emailTextField.text;
     
     [user remindPasswordWithSuccess:^{
-         [self.navigationController popViewControllerAnimated:YES];
+        [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Remind successful", nil)
+                                    message:NSLocalizedString(@"You will receive e-mail with instructions how to reset your password.", nil)
+                                   delegate:self
+                          cancelButtonTitle:[self titleForPasswordRemindAlertCancelButton]
+                          otherButtonTitles:nil] show];
      } failure:^(NSError *error) {
          UIAlertView *errorAlert = [UIAlertView dvs_alertViewForError:error
                                          statusDescriptionsDictionary:@{ @404: NSLocalizedString(@"User for current e-mail does not exist.", nil) }];
          [errorAlert show];
      }];
+}
+
+#pragma mark - UIAlertViewDelegate
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    NSString *buttonTitle = [alertView buttonTitleAtIndex:buttonIndex];
+    if ([buttonTitle isEqualToString:[self titleForPasswordRemindAlertCancelButton]]) {
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+}
+
+- (NSString *)titleForPasswordRemindAlertCancelButton {
+    return NSLocalizedString(@"Close", nil);
 }
 
 @end
