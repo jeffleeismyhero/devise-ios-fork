d86e23d821a4e5b54a26e824fff61ba6350b69b4
Wojceich Trzasko <wojciech.trzasko@gmail.com>
2015-01-05 17:17:19 +0100

    [#85113728] Basic setup for functional tests of demo app.

---
+++ Devise Demo Tests/DVSLogInViewSpec.m
@@ -0,0 +1,19 @@
+//
+//  DVSLogInViewSpec.m
+//  Devise
+//
+//  Created by Wojciech Trzasko on 05.01.2015.
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+SPEC_BEGIN(DVSLogInViewSpec)
+
+describe(@"DVSLogInView", ^{
+    
+    it(@"should do basic test", ^{
+        [[theValue(@1) should] equal:theValue(@1)];
+    });
+    
+});
+
+SPEC_END
