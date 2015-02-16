5630742c9f87eea65b288ebbfdc35d2820d76dfc
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2015-02-16 15:06:18 +0100

    Revert macro [ci skip]

--- Devise/DVSTemplatesViewsUserDataSource.m
+++ Devise/DVSTemplatesViewsUserDataSource.m
@@ -33,7 +33,7 @@ static NSUInteger const DVSUserPasswordMinLength = 5;
                     break;
                     
                 case DVSActionRegistration:
-                    validator.msgTooShort([NSString stringWithFormat:@"should have at least %ld characters.", (unsigned long)DVSUserPasswordMinLength]);
+                    validator.msgTooShort(NSLocalizedFormatString(@"should have at least %ld characters.", (unsigned long)DVSUserPasswordMinLength));
                     break;
                     
                 default:
