7870e5e1d577928dde974527c595093f8ad41d3c
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-03-17 15:19:25 +0100

    Added tests for DVSUser+Persistence [#90265942]

---
+++ Devise Tests/DVSUserPersistenceSpec.m
@@ -0,0 +1,79 @@
+//
+//  DVSUserPersistenceSpec.m
+//  Devise
+//
+//  Created by Radoslaw Szeja on 17/03/15.
+//  Copyright 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "DVSTestUser.h"
+#import "DVSUser+Persistence.h"
+
+
+SPEC_BEGIN(DVSUserPersistenceSpec)
+
+describe(@"DVSUserPersistence", ^{
+
+    __block DVSTestUser *testUser = nil;
+    __block NSData *encodedUser = nil;
+    __block DVSTestUser *decodedUser = nil;
+    
+    afterEach(^{
+        testUser = nil;
+        encodedUser = nil;
+        decodedUser = nil;
+    });
+    
+    context(@"encoded user", ^{
+        
+        __block NSString *identifier = nil;
+        __block NSString *email = nil;
+        __block NSString *sessionToken = nil;
+        __block NSString *name = nil;
+        
+        beforeEach(^{
+            
+            identifier = @"!d3nT!fi3R";
+            email = @"example@address.org";
+            sessionToken = @"t0k3n";
+            name = @"example_name";
+            
+            testUser = [[DVSTestUser alloc] init];
+            testUser.identifier = identifier;
+            testUser.email = email;
+            testUser.password = @"s3cR3t";
+            testUser.sessionToken = sessionToken;
+            testUser.name = name;
+            
+            encodedUser = [NSKeyedArchiver archivedDataWithRootObject:testUser];
+            decodedUser = [NSKeyedUnarchiver unarchiveObjectWithData:encodedUser];
+            
+        });
+        
+        afterEach(^{
+            
+            identifier = nil;
+            email = nil;
+            sessionToken = nil;
+            name = nil;
+            
+        });
+        
+        it(@"should be decoded correctly", ^{
+            [[decodedUser should] equal:testUser];
+        });
+        
+        it(@"should have correct properties", ^{
+            [[decodedUser.identifier    should] equal:identifier];
+            [[decodedUser.email         should] equal:email];
+            [[decodedUser.sessionToken  should] equal:sessionToken];
+            [[decodedUser.name          should] equal:name];
+            [[decodedUser.password      should] beNil];
+        });
+        
+    });
+    
+});
+
+SPEC_END
